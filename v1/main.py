9b75c5b6f93ffe517850ab231e921c5797b926ae
NotNoper <n.attilako@gmail.com>
2024-06-21 12:09:40 +0300

    wait a sec need to restart computer

--- v1/main.py
+++ v1/main.py
@@ -1,6 +1,13 @@
+import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
-x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+fashion_mnist = tf.keras.datasets.fashion_mnist # The MNIST is the dataset of a bunch of clothes in 28 by 28 pixels. It is what the ai is training off
 
-print(x)
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data() # The images are 28 by 28 Numpy arrays, with pixel values ranging from 0 to 255. The labels are an array of integers, ranging from 0 to 9. These correspond to the different kinds of clothing.
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+len(train_labels)
 
