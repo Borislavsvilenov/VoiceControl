24a8181eb220b3f12bc796d600003a51732884b3
Bobislav <bobo.svilenov@gmail.com>
2024-06-22 17:13:49 +0300

    some formating

---
+++ v1/funcs.py
@@ -0,0 +1,5 @@
+def SearchForKeywords(text):
+    firstWord = text.split()[0]
+    print(firstWord)
+
+
--- v1/main.py
+++ v1/main.py
@@ -2,35 +2,32 @@ import speech_recognition as sr
 import sounddevice as sd
 from scipy.io.wavfile import write
 import time
-import os
 
-def SearchForKeywords(text):
-    firstWord = text.split()[0]
-    print(firstWord)
+from funcs import SearchForKeywords
 
 frequency = 44100
 duration = 5
 
 r = sr.Recognizer()
 
-recording = sd.rec(int(duration * frequency), samplerate=frequency, channels=1, dtype='int16')
-sd.wait()
+def main():
+    recording = sd.rec(int(duration * frequency), samplerate=frequency, channels=1, dtype='int16', blocking=True)
+    sd.wait()
 
-sd.wait()
+    sd.wait()
 
-write("recording0.wav", frequency, recording)
+    write("recording0.wav", frequency, recording)
 
-time.sleep(0.2)
+    time.sleep(0.2)
 
-with sr.AudioFile('recording0.wav') as source:
-    audio_text = r.listen(source)
-    try:
-        text = r.recognize_google(audio_text)
-        print(text)
-        os.remove('recording0.wav')
-        SearchForKeywords(text)
-        
-    except:
-         print('Sorry.. run again...')
+    with sr.AudioFile('recording0.wav') as source:
+        audio_text = r.listen(source)
+        try:
+            text = r.recognize_google(audio_text)
+            print(text)
+            SearchForKeywords(text)
+        except:
+            print('Sorry.. run again...')
 
-
+if __name__ == '__main__':
+    main()
