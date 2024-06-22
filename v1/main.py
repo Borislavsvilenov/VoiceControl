c886b40c30a30fd3cf216788f34299db2a8939cd
NotNoper <n.attilako@gmail.com>
2024-06-22 18:24:23 +0300

    This could work?

--- v1/main.py
+++ v1/main.py
@@ -1,30 +1,38 @@
 import speech_recognition as sr
 import sounddevice as sd
 from scipy.io.wavfile import write
-import time
+from gtts import gTTS
+from playsound import playsound
 import os
+import time
+from funcs import SearchForKeywords
 
-def SearchForKeywords(text):
-    firstWord = text.split()[0]
-    print(firstWord)
+def main():
+    frequency = 44100
+    duration = 5
 
 
-frequency = 44100
-duration = 5
+    r = sr.Recognizer()
 
-r = sr.Recognizer()
 
-recording = sd.rec(int(duration * frequency), samplerate=frequency, channels=1, dtype='int16')
-sd.wait()
+    recording = sd.rec(int(duration * frequency), samplerate=frequency, channels=1, dtype='int16')
+    sd.wait()
 
-sd.wait()
 
-write("recording0.wav", frequency, recording)
+    sd.wait()
 
-time.sleep(0.2)
-    
-with sr.AudioFile('recording0.wav') as source:
-    audio_text = r.listen(source)
-    text = r.recognize_google(audio_text)
-    print(text)
-    SearchForKeywords(text)
+
+    write("recording0.wav", frequency, recording)
+
+
+    time.sleep(0.2)
+   
+    with sr.AudioFile('recording0.wav') as source:
+        audio_text = r.listen(source)
+        text = r.recognize_google(audio_text)
+        print(text)
+        SearchForKeywords(text)
+
+
+if __name__ == '__main__':
+    main()
