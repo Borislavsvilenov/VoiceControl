8f7dfc66c687750edcfb39c7a8c78d5178ad644f
NotNoper <n.attilako@gmail.com>
2024-06-23 22:01:21 +0300

    it can open stuff

--- v1/funcs.py
+++ v1/funcs.py
@@ -1,6 +1,20 @@
 import time
 from gtts import gTTS
 from playsound import playsound
+import os
+import subprocess
+
+
+def get_desktop_path():
+    user_profile = os.environ['USERPROFILE']
+    desktop_path = os.path.join(user_profile, 'Desktop')
+    onedrive_desktop_path = os.path.join(user_profile, 'OneDrive', 'Desktop')
+   
+    if os.path.exists(onedrive_desktop_path):
+        return onedrive_desktop_path
+    return desktop_path
+
+
 
 
 def SearchForKeywords(text):
@@ -14,6 +28,7 @@ def SearchForKeywords(text):
         currentTimeNumber = int(currentTime)
         if(currentTimeNumber > 19):
             print("Good Night")
+            TextToSpeech("Good Night")
 
 
         elif(currentTimeNumber > 11):
@@ -22,10 +37,41 @@ def SearchForKeywords(text):
        
         else:
             print("Good Morning")
+            TextToSpeech("Good Morning")
+   
+    elif firstWord == "open":
+        differentWords = text.split()
+        restWords = ' '.join(differentWords[1:])
+       
+        desktop_path = get_desktop_path()
+        print("Desktop path:", desktop_path)
+
+
+        items = os.listdir(desktop_path)
+        found = False
+        for item in items:
+            filename, file_extension = os.path.splitext(item)
+            if restWords.lower() == filename.lower():
+                path_to_open = os.path.join(desktop_path, item)
+                print("Opening:", path_to_open)
+                if os.path.isfile(path_to_open):
+                    os.startfile(path_to_open)
+                else:
+                    subprocess.Popen(f'explorer "{path_to_open}"')
+                found = True
+                break
+
+
+        if not found:
+            print(f"No item found with name: {restWords}")
 
 
 def TextToSpeech(text):
     speech = gTTS(text)
+
+
+    if(os.path.exists('speech.mp3')):
+        os.remove('speech.mp3')
 
 
     speech_file = 'speech.mp3'
--- v1/main.py
+++ v1/main.py
@@ -4,16 +4,20 @@ from scipy.io.wavfile import write
 import time
 from funcs import *
 
+
 def main():
     frequency = 44100
     duration = 5
 
+
     r = sr.Recognizer()
+
 
     recording = sd.rec(int(duration * frequency), samplerate=frequency, channels=1, dtype='int16')
     sd.wait()
     sd.wait()
     write("lastRecord.wav", frequency, recording)
+
 
     time.sleep(0.2)
    
@@ -22,6 +26,7 @@ def main():
         text = r.recognize_google(audio_text)
         print(text)
         SearchForKeywords(text)
+
 
 if __name__ == '__main__':
     main()
